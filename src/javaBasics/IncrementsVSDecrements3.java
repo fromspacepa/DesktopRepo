4b2c14d8af0d1a1016b6b7f8ca3d2c879e74e7de
Turtle$2021 <fhamed932@gmail.com>
2022-02-13 22:44:10 -0500

    lastUpdat

--- src/javaBasics/IncrementsVSDecrements3.java
+++ src/javaBasics/IncrementsVSDecrements3.java
@@ -16,14 +16,17 @@ public class IncrementsVSDecrements3 {
 		System.out.println(x);//This will print value of 'x' as '1' as changed above^ by Decrement/Pre-Decrement.
 //In Increment we have Pre-Increment and Post-Increment:
 		//Pre-Increment:
+		//Pre-Increment first increases the value of variable by 1 and then prints the variable.
+		//For instance, initially the value of variable 'y' below is '2' and 
 		int y = 2;//Here the Variable 'y' is declared and initialized with the assignment of value '2'
 		System.out.println(++y);//This is Pre-Increment, it first increases the value of 'y' by '1' and then prints the variable 'y'
 		//Now the above^ will print value '3' because of Pre-Increment flow execution.
-		System.out.println(y++);//This is Post-Increment, it will still print the value of 'y' as '3' because of the Post-Increment
-		//flow execution.
-		System.out.println(y);
 		
 		//Post-Increment:
+		System.out.println(y++);//This is Post-Increment, it will still print the value of 'y' as '3' because of the Post-Increment
+		//flow of execution.
+		System.out.println(y);//This will print the value of 'y' as '4' because of the above^ Post-Increment flow of execution.
+		
 //The same we have Pre-Decrement and Post-Decrement"
 		//Pre-Decrement:
 		

7c8b149e052099bc26393be9147454648f6e0458
Turtle$2021 <fhamed932@gmail.com>
2022-02-13 20:21:20 -0500

    EditedOnDesktop

--- src/javaBasics/IncrementsVSDecrements3.java
+++ src/javaBasics/IncrementsVSDecrements3.java
@@ -5,11 +5,11 @@ public class IncrementsVSDecrements3 {
 	public static void main(String[] args) {
 
 //Increment VS Decrement:
-	//In Increment we have Pre-Increment and Post-Increment:
+//In Increment we have Pre-Increment and Post-Increment:
 		//Pre-Increment:
 		
 		//Post-Increment:
-	//The same we have Pre-Decrement and Post-Decrement"
+//The same we have Pre-Decrement and Post-Decrement"
 		//Pre-Decrement:
 		
 		//Post-Decrement:

644f171008b50546ce9e03f0d53659a0a0983131
fromspacepa <fhamed932@gmail.com>
2022-02-14 08:55:47 -0500

    UpdatedInLaptop

--- src/javaBasics/IncrementsVSDecrements3.java
+++ src/javaBasics/IncrementsVSDecrements3.java
@@ -4,35 +4,42 @@ public class IncrementsVSDecrements3 {
 
 	public static void main(String[] args) {
 
-//Increment VS Decrement:
-		//Increment will be "++" (plus plus); This also means Increment by 1.
+		//INCREMENT VS DECREMENT:
+//>>>Increment will be "++" (plus plus); This also means Increase variable by 1.
+//>>>Decrement will be "--" (minus minus); This also means Decrease variable by 1.
+//Then in Increment & Decrement we have Pre-Increment & Post-Increment; The same we have Pre-Decrement & Post-Decrement:
+
+		//>>>PRE-INCREMENT:
+//Pre-Increment first increases the value of variable by 1 and then prints the variable.
+System.out.println("The flow execution of Pre-Increment:");
 		int x = 1;//Here the Variable 'x' is declared and initialized with the assignment of value '1'
-		System.out.println(x);//This will print value of 'x' as '1'.
-		System.out.println(++x);//This will print the value of 'x' as '2' because of Increment/Pre-Increment.
-		System.out.println(x);//This will still print value of 'x' as '2' as changed above^ by Increment/Pre-Increment.
-		
-		//Decrement will be "--" (minus minus); This also means Decrement by 1.
-		System.out.println(--x);//This will print value of 'x' as '1' because of Decrement/Pre-Decrement.
-		System.out.println(x);//This will print value of 'x' as '1' as changed above^ by Decrement/Pre-Decrement.
-//In Increment we have Pre-Increment and Post-Increment:
-		//Pre-Increment:
-		//Pre-Increment first increases the value of variable by 1 and then prints the variable.
-		//For instance, initially the value of variable 'y' below is '2' and 
-		int y = 2;//Here the Variable 'y' is declared and initialized with the assignment of value '2'
-		System.out.println(++y);//This is Pre-Increment, it first increases the value of 'y' by '1' and then prints the variable 'y'
-		//Now the above^ will print value '3' because of Pre-Increment flow execution.
-		
-		//Post-Increment:
-		System.out.println(y++);//This is Post-Increment, it will still print the value of 'y' as '3' because of the Post-Increment
-		//flow of execution.
-		System.out.println(y);//This will print the value of 'y' as '4' because of the above^ Post-Increment flow of execution.
-		
-//The same we have Pre-Decrement and Post-Decrement"
-		//Pre-Decrement:
-		
-		//Post-Decrement:
-		
+		System.out.println(x);//This will print the value of above^ Variable 'x' as '1'.
+		System.out.println(++x);//This is Pre-Increment, it will print the value of 'x' as '2' because of flow execution of Pre-Increment.
+		System.out.println(x);//Now it will still print the value of 'x' as '2' because of changes happened above^ by the flow execution of Pre-Increment.
 	
+		//>>>POST-INCREMEMNT:
+//Post-Increment first prints the variable and then increase the value of variable.
+System.out.println("The flow execution of Post-Increment:");
+		int y = 2;//Here the Variable 'y' is declared and initialized with the assignment of value '2'
+		System.out.println(y);//This will print the value of above^ Variable 'y' as '2'
+		System.out.println(y++);//This is Post-Increment, it will still print the value of 'y' as '2' because of the flow execution of Post-Increment.
+		System.out.println(y);//Now it will print the value of 'y' as '3' because of changes happened above^ by the flow execution of Post-Increment.
+		
+		//>>>PRE-DECREMENT:
+//Pre-Decrement first decreases the value of variable by 1 and then prints the variable.
+System.out.println("The flow execution of Pre-Decrement:");
+		int a = 3;//Here the Variable 'a' is declared and initialized with the assignment of value '3'
+		System.out.println(a);//This will print the value of above^ Variable 'b' as '3'.
+		System.out.println(--a);//This is Pre-Decrement, it will print the value of 'b' as '2' because of flow execution of Pre-Decrement.
+		System.out.println(a);//Now it will still print the value of 'b' as '2' because of changes happened above^ by the flow execution of Pre-Decrement.
+	
+		//>>>POST-DECREMEMNT:
+//Post-Decrement first prints the variable and then increase the value of variable.
+System.out.println("The flow execution of Post-Decrement:");
+		int b = 4;//Here the Variable 'b' is declared and initialized with the assignment of value '4'
+		System.out.println(b);//This will print the value of above^ Variable 'b' as '4'
+		System.out.println(b--);//This is Post-Decrement, it will still print the value of 'b' as '4' because of the flow execution of Post-Decrement.
+		System.out.println(b);//Now it will print the value of 'b' as '3' because of changes happened above^ by the flow execution of Post-Decrement.
 		
 
 	}

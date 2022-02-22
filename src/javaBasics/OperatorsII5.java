4ee33164bdb72beccead4e424e3dd2ae0f7c01b3
fromspacepa <fhamed932@gmail.com>
2022-02-22 14:25:25 -0500

    LaptopUpdates

--- src/javaBasics/OperatorsII5.java
+++ src/javaBasics/OperatorsII5.java
@@ -3,7 +3,23 @@ package javaBasics;
 public class OperatorsII5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+//Equality Operators:
+		// "==" >>> Means: Equal to
+		// "!=" >>> Means: Not Equal to
+		
+//Difference between Equal/Equality Operators used with Primitive Data Types & Non-Primitive/Reference Data Types:
+		//In general Equal/Equality Operators will be used with Primitive Data Types; Basically while using Equal/Equality Operators with Primitive Data Types,
+		//they check the values of the Primitive Data Types in the Stack; to see the difference value between two Primitive Data Types.
+		//But while using Equal OR Equality Operators with Non-Primitive/Reference Data Types, they basically will check if both the Non-Primitive/Reference Data Types
+		//are saved in the same location in the Memory.
+		
+		int x = 25;
+		int y = 15;
+		
+		
+		System.out.println(x == y);
 
 	}
 

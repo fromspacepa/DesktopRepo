843c20387667d6dbdc7d5f890127f4df061e5ec4
fromspacepa <fhamed932@gmail.com>
2022-02-23 08:31:47 -0500

    LaptopUpdated

--- src/javaBasics/OperatorsII5.java
+++ src/javaBasics/OperatorsII5.java
@@ -11,15 +11,16 @@ public class OperatorsII5 {
 		
 //Difference between Equal/Equality Operators used with Primitive Data Types & Non-Primitive/Reference Data Types:
 		//In general Equal/Equality Operators will be used with Primitive Data Types; Basically while using Equal/Equality Operators with Primitive Data Types,
-		//they check the values of the Primitive Data Types in the Stack; to see the difference value between two Primitive Data Types.
-		//But while using Equal OR Equality Operators with Non-Primitive/Reference Data Types, they basically will check if both the Non-Primitive/Reference Data Types
-		//are saved in the same location in the Memory.
+		//they will check the values of the Primitive Data Types in the Stack; to see if the values are Equal/Same OR Not Equal/Same.
+		//But while using Equal/Equality Operators with Non-Primitive/Reference Data Types, they basically will check if both the Non-Primitive/Reference Data Types
+		//are stored/saved OR not stored/saved in the same location in the Memory.
 		
-		int x = 25;
-		int y = 15;
+		int x = 25;//Here the Value of "25" is Initialized and assigned to Variable "X"; Which is changeable at any time.
+		int y = 15;//Here the Value of "15" is Initialized and assigned to Variable "Y"; Which is changeable at any time.
 		
-		System.out.println(x == y);
-		System.out.println(x != y);
+		System.out.println(x == y);//This will printout the boolean result (if "X" is equal to "Y" it will print "true", if not it will print "false").
+		System.out.println(x != y);//This will printout the boolean result (if "X" is not equal to "Y" it will print "true", if not it will print "false").
+		
 
 	}
 

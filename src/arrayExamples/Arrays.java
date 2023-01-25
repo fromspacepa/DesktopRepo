ad441da3e6997197baba4d5ce2319bbd47967d56
Turtle$2021 <fhamed932@gmail.com>
2023-01-24 21:22:57 -0500

    PCUpdates12423

--- src/arrayExamples/Arrays.java
+++ src/arrayExamples/Arrays.java
@@ -32,17 +32,23 @@ public class Arrays {
 		data [3] = 10;
 		data [4] = 5;
 		
-		System.out.println("-------------------------");
+
 		//This is how to print the length of elements in a single dimension Array:
-		System.out.println("Single dimension Array with \nelements of:\t\t\t" + data.length);//the printout is going to be 5.
+		System.out.println("--------Single dimension--------\n--integer Arrays with 5 elements--");//the printout is going to be 5.
+		System.out.println("Numbers of Elements:\t\t" + data.length);
 
 		//This is how to print the value of index 0:
 		System.out.println("Value of index '0':\t\t" + data[0]);
+		System.out.println("Value of index '1':\t\t" + data[1]);
+		System.out.println("Value of index '2':\t\t" + data[2]);
+		System.out.println("Value of index '3':\t\t" + data[3]);
+		System.out.println("Value of index '4':\t\t" + data[4]);
+		System.out.println("\n---Value of indexes from 0 to 4 by order:");
 		//This is how to printout the value of all indexes:
 		for (int i = 0; i < 5; i++ ) {
-			System.out.println("Value of indexes from 0 to 4:\t" + data[i]);
+			System.out.println(data[i]);
 		}
-		
+		System.out.println("\n---Examples of String Arrays---");
 		//This is how to creat a single dimension Array of String:
 		String[] names = new String [5];
 		names[0] = "Ahmad";
@@ -50,7 +56,7 @@ public class Arrays {
 		names[2] = "Kabir";
 		names[3] = "Wali";
 		names[4] = "Sardar";
-		System.out.println("Number of Names: " + names.length);
+		System.out.println("Number of Names and Elements: " + names.length);
 		for (int i = 0; i <= 4; i++) {
 			System.out.println("List of all Names: " + names[i]);
 		}

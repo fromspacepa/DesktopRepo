9d96fc4b5fe861925eae003e50b4768d6e8196f5
fromspacepa <fhamed932@gmail.com>
2023-01-27 14:38:04 -0500

    LaptopUpdates12723

--- src/arrayExamples/CountSumMinMaxAvgOfElementsValuesInArrays.java
+++ src/arrayExamples/CountSumMinMaxAvgOfElementsValuesInArrays.java
@@ -1,35 +1,45 @@
 package arrayExamples;
 
+import java.text.DecimalFormat;
+
 public class CountSumMinMaxAvgOfElementsValuesInArrays {
 
 	public static void main(String[] args) {
 
 
-		int[] numbers = {12, 34, 23, 21, 10, 50, 65, 67, 86};
+		int[] numbers = {12, 34, 23, 21, 3, 50, 65, 67, 86};
 		
 		int count = numbers.length;//This is going to give us the length of Arrays (Elements in Arrays).
-		System.out.println(numbers.length);
-		System.out.println("-------------------------------");
 		int sum = 0;
 		int min = numbers[0];
-		//System.out.println(numbers[0]);
 		int max = numbers[0];
-		double ave = 0;
+		double avg = 0;
+		DecimalFormat numberFormat = new DecimalFormat ("#0.00");
 		
+			
 		for (int i = 0; i < numbers.length; i++) {
-			//To find sum:
-			//sum = sum + numbers[i];
-			// OR
-			sum += numbers[i];
-			System.out.println(sum);
-			//System.out.println(numbers[i]);
+			sum += numbers[i];//To find the sum (total) of numbers in the Array.
+			if (numbers[i] < min) {
+				min = numbers[i];
+			}else if (numbers[i] > max) {
+				max = numbers[i];
+					}
 		}
-		System.out.println(sum);
-		{
-			for (int i: numbers) {
-				System.out.println(i);
+		avg = sum / count;
+		System.out.println("-------------------------------");
+		System.out.println("The Number of Elements in Arrays:\t" + count);
+		System.out.println("The Sum of Arrays:\t\t\t" + sum);
+		System.out.println("The Minimum of Arrays:\t\t\t" + min);
+		System.out.println("The Maximum of Arrays:\t\t\t" + (max));
+		System.out.println("The Average of Arrays:\t\t\t" + numberFormat.format(avg));
+		
+		
+
+		//System.out.println(numbers[0]);
+		
+
+
 		}
+	
 
-	}
-
-}}
+}

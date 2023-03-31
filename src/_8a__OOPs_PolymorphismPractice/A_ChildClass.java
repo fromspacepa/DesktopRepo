6c67f2e2ae8217eeef1cafad614f70a4c3cf287f
fromspacepa <fhamed932@gmail.com>
2023-03-31 11:37:24 -0400

    LaptopUpdates33123

--- src/_8a__OOPs_PolymorphismPractice/A_ChildClass.java
+++ src/_8a__OOPs_PolymorphismPractice/A_ChildClass.java
@@ -1,5 +1,50 @@
 package _8a__OOPs_PolymorphismPractice;
 
-public class A_ChildClass {
+public class A_ChildClass extends ParentClass{
+	
+	private int age;
+	private String phoneNo;
+	private String major;
+	
+	public A_ChildClass() {
+		super();
+		this.age = 0;
+		this.phoneNo = "";
+		this.major = "";
+		
+	}
+	public A_ChildClass(String firstName, String lastName, String dateOfBirth, String nationality, int age, String phoneNo, String major) {
+		super(firstName, lastName, dateOfBirth, nationality);
+		this.age = age;
+		this.phoneNo = phoneNo;
+		this.major = major;
+		
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
 
+	@Override
+	public String toString() {
+		String sep = "\n***********************************\n";
+		String title = "Child Class___A:";
+		String temPhone = "(" + phoneNo.substring(0, 3) + ") " + phoneNo.subSequence(3, 6) + "-"+ phoneNo.substring(6, 10); 
+		return title + sep + super.toString() + "\nAge:\t\t" + age + "\nPhone No:\t" + temPhone + "\nMajor:\t\t" + major;
+		
+	}
 }
--- src/_8a__OOPs_PolymorphismPractice/ParentClass.java
+++ src/_8a__OOPs_PolymorphismPractice/ParentClass.java
@@ -1,7 +1,63 @@
 package _8a__OOPs_PolymorphismPractice;
 
+import java.io.PrintStream;
+
 public class ParentClass {
 	
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String nationality;
+
+	public ParentClass() {
+		super();
+		firstName = "";
+		lastName = "";
+		dateOfBirth = "";
+		nationality = "";
+		
+	}
+	public ParentClass(String firstName, String lastName, String dateOfBirth, String nationality) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.nationality = nationality;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+				
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	
+	@Override
+	public String toString() {
+		String sep = "\n***********************************";
+		String title = "Parent Class:";
+		String formatDOB = dateOfBirth.substring(0, 2) + "/" + dateOfBirth.substring(2, 4) + "/" + dateOfBirth.substring(4, 8);
+		return title + sep + "\nFirst Name:\t" + firstName + "\nLast Name:\t"+ lastName + "\nDate of Birth:\t" + formatDOB + "\nNationality:\t" + nationality;
+	}
+
+		
 	
 
 }

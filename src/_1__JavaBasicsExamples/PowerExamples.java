88dbe5b5e0ef54bbdce062f222b3ec7b740f05d7
fromspacepa <fhamed932@gmail.com>
2023-04-25 13:18:40 -0400

    LaptopUpdates42423

---
+++ src/_1__JavaBasicsExamples/PowerExamples.java
@@ -0,0 +1,15 @@
+package _1__JavaBasicsExamples;
+
+public class PowerExamples {
+
+	public static void main(String[] args) {
+
+
+		int myNum = 5;
+		int myValue = 5;
+		
+		System.out.println(myNum ^ myValue);
+
+	}
+
+}

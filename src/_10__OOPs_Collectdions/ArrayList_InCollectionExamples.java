dfc9ac9d657d4687fed9181b5fe333796a2d65df
fromspacepa <fhamed932@gmail.com>
2023-05-19 10:04:25 -0400

    LaptopUpdates51923

---
+++ src/_10__OOPs_Collectdions/ArrayList_InCollectionExamples.java
@@ -0,0 +1,21 @@
+package _10__OOPs_Collectdions;
+
+import java.util.ArrayList;
+
+public class ArrayList_InCollectionExamples {
+
+	public static void main(String[] args) {
+
+
+		ArrayList <String> aL = new ArrayList ();
+		aL.add("Afghan");
+		aL.add("123");
+		
+		//aL.remove(0);
+		
+		System.out.println(aL.get(0));
+		
+
+	}
+
+}
---
+++ src/_10__OOPs_Collectdions/LinkedList_InCollectionExamples.java
@@ -0,0 +1,23 @@
+package _10__OOPs_Collectdions;
+
+import java.util.LinkedList;
+
+public class LinkedList_InCollectionExamples {
+
+	public static void main(String[] args) {
+
+
+		
+		LinkedList <String> ls = new LinkedList ();
+		ls.add("Habibi");
+		ls.add("123");
+		
+		ls.remove(0);
+		
+
+
+
+
+	}
+
+}

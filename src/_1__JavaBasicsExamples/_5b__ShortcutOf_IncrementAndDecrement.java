55c9b4f4984d9e60ceb99d60f11db2110f1a650a
fromspacepa <fhamed932@gmail.com>
2023-05-24 13:26:53 -0400

    LaptopUpdates52423

---
+++ src/_1__JavaBasicsExamples/_5b__ShortcutOf_IncrementAndDecrement.java
@@ -0,0 +1,74 @@
+package _1__JavaBasicsExamples;
+
+public class _5b__ShortcutOf_IncrementAndDecrement {
+	
+	public static void main(String[] args) {
+		
+		
+		/** 
+		 * >>> INCREMENT & DECREMENT Prefix:
+		 * -> Increment means increasing value of a variable by '1' one.
+		 * -> Decrement means decreasing value of a variable by '1' one.
+		 * 
+		 * ___Prefix Increment (PreIncrement) "++x": means that the value of a variable is increased, before calling it.
+		 * ___Prefix Decrement (PreDecrement) "--x": means that the value of a variable is decreased, before calling it.
+		 * 
+		 * Following here are the examples of using PreIncrement and PreDecrement:
+		 * 
+		 */
+		
+		System.out.println("\n...Calling the PreIncrement & PreDecrement Shortcuts:\n");
+		
+		int numA = 10;
+		int numB = 20;
+		Double numC = 30.25;
+		++numA; 
+		int z = ++numA / 11;//Just a different example
+		System.out.println("Variable Initially Declared & Initialized:\t"+"numA:"+ numA +"\tnumB:"+ numB +"\tnumC:"+ numC);
+		System.out.println("Variable during PrefixIncrement Applied:\t"+"numA:"+ ++numA +"\tnumB:"+ ++numB +"\tnumC:"+ ++numC);
+		System.out.println("Variable after PrefixIncrement Applied:\t\t" +"numA:"+ numA +"\tnumB:"+ numB +"\tnumC:"+ numC);
+		System.out.println("Variable during PrefixDecrement Applied:\t"+"numA:"+ --numA +"\tnumB:"+ --numB +"\tnumC:"+ --numC);
+		System.out.println("Variable after PrefixDecrement Applied:\t\t" +"numA:"+ numA +"\tnumB:"+ numB +"\tnumC:"+ numC);
+		
+		
+		/** 
+		 * >>> INCREMENT & DECREMENT Postfix:
+		 * -> Increment means increasing value of a variable by '1' one.
+		 * -> Decrement means decreasing value of a variable by '1' one.
+		 * 
+		 * ___Postfix Increment (PostIncrement) "x++": means that the value of a variable is increased, after calling it.
+		 * ___Postfix Decrement (PostDecrement) "x--": means that the value of a variable is decreased, after calling it.
+		 * 
+		 * Following here are the examples of using PreIncrement and PreDecrement:
+		 * 
+		 */
+		
+		System.out.println("\n...Calling the PostIncrement & PostDecrement Shortcuts:\n");
+		
+		int numD = 10;
+		int numE = 20;
+		Double numF = 30.25;
+		System.out.println("Variable Initially Declared & Initialized:\t"+"numD:"+ numD +" numE:"+ numE +" numF:"+ numF);
+		System.out.println("Variable during PostfixIncrement Applied:\t"+"numD:"+ numD++ +" numE:"+ numE++ +" numF:"+ numF++);
+		System.out.println("Variable after PostfixIncrement Applied:\t" +"numD:"+ numD +" numE:"+ numE +" numF:"+ numF);
+		System.out.println("Variable during PostfixDecrement Applied:\t"+"numD:"+ numD-- +" numE:"+ numE-- +" numF:"+ numF--);
+		System.out.println("Variable after PostfixDecrement Applied:\t" +"numD:"+ numD +" numE:"+ numE +" numF:"+ numF);
+		
+		
+		//>>>>> What is the outcome of following question?
+		System.out.println("\nYou could find out where these coming from:");
+		int a = 5;
+		int b = 6;
+		int c;
+		int d;
+		c = ++a - b++ + a++;
+		System.out.println(a);
+		d = a * b % a + b++ - b + ++b - 7;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
--- src/_1__JavaBasicsExamples/_5d__MixedTypeArithmeticExpressionsAndTypeCasting.java
+++ src/_1__JavaBasicsExamples/_5d__MixedTypeArithmeticExpressionsAndTypeCasting.java
@@ -1,6 +1,6 @@
 package _1__JavaBasicsExamples;
 
-public class _5c__MixedTypeArithmeticExpressionsAndTypeCasting {
+public class _5d__MixedTypeArithmeticExpressionsAndTypeCasting {
 
 	public static void main(String[] args) {
 		

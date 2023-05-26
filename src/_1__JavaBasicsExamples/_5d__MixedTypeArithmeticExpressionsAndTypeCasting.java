ab299bdc060b8c7da67a096dba177da5703882f7
fromspacepa <fhamed932@gmail.com>
2023-05-26 14:28:53 -0400

    LaptopUpdates52623

--- src/_1__JavaBasicsExamples/_5d__MixedTypeArithmeticExpressionsAndTypeCasting.java
+++ src/_1__JavaBasicsExamples/_5d__MixedTypeArithmeticExpressionsAndTypeCasting.java
@@ -14,7 +14,8 @@ public class _5d__MixedTypeArithmeticExpressionsAndTypeCasting {
 		 *   	__Implicite Casting
 		 *    	__Explicite Casting
 		 *    
-		 * 1) Mixed Type is when you are trying to do an operation between double and integer values,
+		 * 1) Mixed Type: 
+		 * is when you are trying to do an operation between double and integer values,
 		 * while doing Mixed Type Arithmetic Execution in your code such as "int <--> double" it will give you the
 		 * result of double value.
 		 * NOTE: Just keep in mind that the double data type is more accurate then integer data type.
@@ -26,59 +27,62 @@ public class _5d__MixedTypeArithmeticExpressionsAndTypeCasting {
 		
 		// With the execution of Mixed Type Addition, this will give you the result of 9.7:
 		double sum = x + y;
-		System.out.println("The outcome for Mixed-Type execution of 'doubl --> int':\n.......... " + sum);
+		System.out.println("The outcome for Mixed-Type execution of 'doubl <--> int':\n.......... " + sum);
 		// With the execution of Mixed-Type Multiplication, this will give you the result of 20.1:
 		double multiplication = x * y;
-		System.out.println("The outcome for Mixed-Type execution of 'double --> int':\n.......... " + multiplication);
+		System.out.println("The outcome for Mixed-Type execution of 'double <--> int':\n.......... " + multiplication);
 		// With the execution of Mixed-Type Division this will give the result of 2.2333333333333334
 		double division = x / y;
-		System.out.println("The outcome for Mixed-Type execution of 'double --> int':\n.......... " + division);
+		System.out.println("The outcome for Mixed-Type execution of 'double <--> int':\n.......... " + division);
 		
-		/** >>>>> Mixed-Type Arithmetic Expressions CONTINUES....:
+		/** 
+		 * 2) Mixed Type with Promoting: 
+		 * is while doing mixed execution of integer data type to double data type; In Java this term is called promotion,
+		 * because the value of integer is promoting to double; means that the integer value will get promoted to double with decimals.
 		 * 
-		 * >>>MIXED-TYPE WITH PROMOTING:  (2-OF-3)
-		 * ___While doing Mixed-Type Arithmetic Execution such as integer <--> double, here this term is called "Promotion"
-		 * because the value of integer is promoting to double; means that the integer value will promote to double value (with decimals).
-		 * Examples are provided below: */
+		 * Following are the examples of Promotion:
+		 */
 				
-		byte byteValue = 127;
-		short shortValue = 30000;
-		int integerValue = 100000;
-		float floatValue = 2000.12f;
-		double doubleValue = 1000.24;
-		long longValue = 2000002325234444442L;
+		byte byteVal = 127;
+		short shortVal = 30000;
+		int integerVal = 100000;
+		float floatVal = 2000.12f;
+		double doubleVal = 1000.24;
+		long longVal = 2000002325234444442L;
 		char charCharacter = 'B';//The Value of Character "B" is 66. (Referenced from ASCII Table)
 		
 		System.out.println("\nFollowing are the results of Mixed-Type Promotion Operations:\n");
-		double promotingToDouble = byteValue + floatValue;//This is when you promoting byte <--> float to double: (2127.1201171875)
-		System.out.println("Promoting of byte <--> float resulting to double: \n\t" + promotingToDouble);
-		float promotingToFloat = byteValue + longValue;//This is when you promoting byte <--> long to float: (2.00000231E18)
-		System.out.println("Promoting of byte <--> long resulting to float: \n\t" + promotingToFloat);
-		long promotingToLong = byteValue + integerValue;//This is when you promoting byte <--> integer to long: (100127)
-		System.out.println("Promoting of byte <--> int resuling to long: \n\t" + promotingToLong);
-		int promotingToInteger = byteValue + shortValue + charCharacter;//This is when you promoting byte <--> short/char to integer: (30193)
-		System.out.println("Promoting of byte <--> short plus char resulting to integer: \n\t" + promotingToInteger);
+		double promotToDouble = byteVal + floatVal;//This is when you promoting byte <--> float to double: (2127.1201171875)
+		System.out.println("Promoting of byte <--> float resulting to double: \n\t" + promotToDouble);
+		float promotToFloat = byteVal + longVal;//This is when you promoting byte <--> long to float: (2.00000231E18)
+		System.out.println("Promoting of byte <--> long resulting to float: \n\t" + promotToFloat);
+		long promotToLong = byteVal + integerVal;//This is when you promoting byte <--> integer to long: (100127)
+		System.out.println("Promoting of byte <--> int resuling to long: \n\t" + promotToLong);
+		int promotToInt = byteVal + shortVal + charCharacter;//This is when you promoting byte <--> short/char to integer: (30193)
+		System.out.println("Promoting of byte <--> short plus char resulting to integer: \n\t" + promotToInt);
 		
-		/** >>>>> Mixed-Type Arithmetic Expressions CONTINUES....:
+		/** 
+		 * 3) Type Casting:
+		 *    __Implicit Casting
+		 *    __Explicit Casting
 		 * 
-		 * >>>TYPE CASTING (3-OF-3)
-		 * ___Implicit Casting
-		 * ___Explicit Casting
+		 * -> Type Casting:
+		 * means converting one data type to another data type; For instance, you convert integer to double and vice versa.  
+		 * __Implicit Casting:
+		 * happens automatically when converting from narrower range data type to a wider range data type without losing data, 
+		 * such as: byte -> short -> char -> int -> long -> float -> double; For instance: converting int to double or float/long.
+		 * NOTE:
+		 * In the above case, Implicit Casting happens automatically because the double/float/long has wider range then integer OR  
+		 * we can say that Implicit Casting happens, when converting a narrower to wider range data type, without losing data.
 		 * 
-		 * ___Casting means converting a data type to another data type; 
-		 * For example you can convert an integer to double and double to integer.
-		 * ___Implicit: --Implicit casting happens automatically when converting from a narrower rage data type 
-		 * to a wider range data type; Such as: byte -> short -> char -> int -> long -> float -> double 
-		 * For instance: Converting int to a double/float/long OR converting float to a double.
-		 * In the above case the Implicit Casting can happen automatically because the double/float/long 
-		 * has wider range than integer OR we can say that Implicit Casting can happen 
-		 * when we convert a narrower data type to another wider range data type without losing data .
-		 * ___Explicit: --Explicit casting does not happen automatically. It should be done by the programmer
-		 * when converting from a wider range data type to a narrower data type; 
-		 * Such as: double -> float -> long -> int -> char -> short -> byte
-		 * For instance: Converting a double/float/long to int then Implicit casting can not happen because you will get exception/error. */
-		
-		//Implicit Casting Examples:
+		 * __Explicit Casting: 
+		 * does not happen automatically, it should be done by the programmer when converting from a wider to narrow range data type,
+		 * such as: double -> float -> long -> int -> char -> short -> byte; For instance converting a double/float/long to int.
+		 * NOTE:
+		 * In the above case, Explicit Casting cannot happen because you will get exception/error during runtime.
+		 */
+
+		// Following are the examples of Implicit Casting:
 		System.out.println("Examples of Implicit casting 'A narrow range data type to a wider range data type:\n");
 		
 		byte myByte = 127;
@@ -89,36 +93,43 @@ public class _5d__MixedTypeArithmeticExpressionsAndTypeCasting {
 		float myFloat = 700.F;
 		double myDouble = 200.22;
 		
-		byte num1 = myByte;//Assigning byte data type to byte Implicitly.
+		// Assigning byte data type to byte Implicitly.
+		byte num1 = myByte;
 		System.out.println("Assigning byte to byte:\n___"+ num1);
-		short num2 = myByte;//Converting byte data type to short Implicitly.
+		// Converting byte data type to short Implicitly.
+		short num2 = myByte;
 		System.out.println("Converting byte to short:\n___"+ num2);
-		char num4 = myChar;//Assigning char data type to char Implicitly.
+		// Assigning char data type to char Implicitly.
+		char num4 = myChar;
 		System.out.println("Assigning char to char:\n___"+ num4);
-		int num6 = myChar, num7 = myShort, num8 = myByte;//Converting char/short/byte to integer Implicitly
+		// Converting char/short/byte to integer Implicitly
+		int num6 = myChar, num7 = myShort, num8 = myByte;
 		System.out.println("Converting char/short/byte to integer:\n___"+ num6 +"_"+ num7 +"_"+ num8);
-		long num11 = myInt, num12 = myChar, num13 = myShort, num14 = myByte;//Converting integer/char/short/byte to long Implicitly.
+		// Converting integer/char/short/byte to long Implicitly.
+		long num11 = myInt, num12 = myChar, num13 = myShort, num14 = myByte;
 		System.out.println("Converting int/char/short/byte to long:\n___"+ num11 +"_"+ num12 +"_"+ num13 +"_"+ num14);
-		float num15 = myLong, num17 = myInt, num18 = myChar, num19 = myShort, num20 = myByte;//Converting long/integer/char/short/byte to float implicitly.
-        System.out.println("Converting long/int/char/short/byte to float:\n___"+ num15 +"_"+ num17 +"_"+ num18 +"_"+ num19 +"_"+ num20);	
-		double num21 = myFloat, num22 = myLong, num23 = myInt, num24 = myChar, num25 = myShort, num26 = myByte;//Converting float/long/
+		// Converting long/integer/char/short/byte to float implicitly.
+		float num15 = myLong, num17 = myInt, num18 = myChar, num19 = myShort, num20 = myByte;
+        System.out.println("Converting long/int/char/short/byte to float:\n___"+ num15 +"_"+ num17 +"_"+ num18 +"_"+ num19 +"_"+ num20);
+        // Converting float/long/integer/char/short/byte to double Implicitly.
+		double num21 = myFloat, num22 = myLong, num23 = myInt, num24 = myChar, num25 = myShort, num26 = myByte;
 		System.out.println("Converting float/long/int/char/short/byte to double:\n___"+ num21 +"_"+ num22 +"_"+ num23 +"_"+ num24 +"_"+ num25 +"_"+ num26);
-		//integer/char/short/byte to double Implicitly.
+	
 		
-		//Explicit Casting Examples:
+		// Following are the examples of Explicit Casting:
 		System.out.println("\n\nExamples of Explicitly Casting 'A wider range data type to a narrow range data type:\n");
-		byte numF = (byte) myDouble;//Explicit Casting of double data to byte (Wider Range to Narrow Rage).
-		short numE = (short) myDouble;//Explicit Casting of double data to short (Wider Range to Narrow Rage).
-		char numD = (char) myDouble;//Explicit Casting of double data to char (Wider Range to Narrow Rage).
-		int numA = (int) myDouble;//Explicit Casting of double data to integer (Wider Range to Narrow Rage).
-		long numB = (long) myDouble;//Explicit Casting of double data to long (Wider Range to Narrow Rage).
-		float numC = (float) myDouble;//Explicit Casting of double data float (Wider Range to Narrow Rage).
+		byte numF = (byte) myDouble;// Explicit Casting of double data to byte (Wider Range to Narrow Rage).
+		short numE = (short) myDouble;// Explicit Casting of double data to short (Wider Range to Narrow Rage).
+		char numD = (char) myDouble;// Explicit Casting of double data to char (Wider Range to Narrow Rage).
+		int numA = (int) myDouble;// Explicit Casting of double data to integer (Wider Range to Narrow Rage).
+		long numB = (long) myDouble;// Explicit Casting of double data to long (Wider Range to Narrow Rage).
+		float numC = (float) myDouble;// Explicit Casting of double data float (Wider Range to Narrow Rage).
 		System.out.println("Converting double to byte/short/char/int/long/float:\n___"+ numF +"_"+ numE +"_"+ numD +"_"+ numA +"_"+ numB +"_"+ numC);
-		long numG = (long) myFloat;//Explicit Casting of float data to long (Wider Range to Narrow Rage).
-		int numH = (int) myFloat;//Explicit Casting of float data to integer (Wider Range to Narrow Rage).
-		char numI = (char) myFloat;//Explicit Casting of float data to char (Wider Range to Narrow Rage).
-		short numJ = (short) myFloat;//Explicit Casting of float data to short (Wider Range to Narrow Rage).
-		byte numK = (byte) myFloat;//Explicit Casting of float data to byte (Wider Range to Narrow Rage).
+		long numG = (long) myFloat;// Explicit Casting of float data to long (Wider Range to Narrow Rage).
+		int numH = (int) myFloat;// Explicit Casting of float data to integer (Wider Range to Narrow Rage).
+		char numI = (char) myFloat;// Explicit Casting of float data to char (Wider Range to Narrow Rage).
+		short numJ = (short) myFloat;// Explicit Casting of float data to short (Wider Range to Narrow Rage).
+		byte numK = (byte) myFloat;// Explicit Casting of float data to byte (Wider Range to Narrow Rage).
 		System.out.println("Converting float to long/int/char/short/byte:\n___"+ numG +"_"+ numH +"_"+ numI +"_"+ numJ +"_"+ numK);
 	} 
 
--- src/_1__JavaBasicsExamples/_6__LogicalOperators_OR_LogicalGets.java
+++ src/_1__JavaBasicsExamples/_6__LogicalOperators_OR_LogicalGets.java
@@ -7,109 +7,112 @@ public class _6__LogicalOperators_OR_LogicalGets {
 
 		String hi = "Welcome";
 		
-		/** >>>>> LOGICAL OPERATORS OR LOGICAL GETS:
+		/** 
+		 * >>> LOGICAL OPERATORS OR LOGICAL GETS:
+		 * Logical Operators are used to determine the logic between variables or values and all Logical Operators are good to use in the 
+		 * "if statements", to make the "if statements" more complicated.
+		 * 
+		 * Following are all the Logical Operators/Gets:
 		 * 
 		 * __Operator:_____Name:__________Description:
-		 * 1)__&&____________Logical AND____Returns TRUE if both side statements are true.
-		 * 2)__||____________Logical OR_____Returns TRUE if one or both side statements are true.
-		 * 3)__!_____________Logical NOT____Reverses the result; Returns FALSE if the result is TRUE.
-		 * 4)__^_____________Logica XOR_____If both sides of the XOR '^' are different the return result will be TRUE.
+		 * 1)__&&__________Logical AND____Returns TRUE if both side statements are true.
+		 * 2)__||__________Logical OR_____Returns TRUE if one or both side statements are true.
+		 * 3)__!___________Logical NOT____Reverses the result; Returns FALSE if the result is TRUE.
+		 * 4)__^___________Logica XOR_____If both sides of the XOR '^' are different the return result will be TRUE.
 		 * _______________________________If both sides of the XOR '^' are the same/true value, the return result will be FALSE.
 		 * _______________________________XOR '^' Means Exclusive OR.
-		 * NOTE: In whole Logical Operators are used to determine the logic between variables or values.
-		 * and all Logical Operators are good to use in the 'if statements' to make it more complicated.
-		 * Following are the examples of all above mentioned Logical Operators/Gets:
 		 * 
-		 * 1) >>> LOGICAL AND '&& EXAMPLES:'
-		 * As explained above, To return 'true' we need to have both sides of the Logical And '&&' operator to be true.
-		 * if one or both sides are false it will return 'false'. Examples are shown below:
-		 *  
+		 * 1) Logical "AND (&&)" Operator:
+		 * NOTE:
+		 * To return "true" we need to have both sides of the Logical "AND (&&)" Operator to be true. 
+		 * If one or both sides of the Logical "AND (&&)" are false, it will return the boolean result of "false".
+		 * 
+		 * Following are the examples of Logical "AND (&&)":
 		 */
 		
 		int x = 20; 
-		//Examples with simple printout:
-		System.out.println("Return resutls for LOGICAL AND '&&':");
+		System.out.println("1)__Return resutls for Logical \"AND (&&)\" Operator:");
 		System.out.println(x < 5 && x < 10);
 		System.out.println(x > 5 && x > 10);
 		System.out.println(x > 10 && x > 20);
+		// Example of Logical "AND (&&)" Operator with boolean data type:
+		System.out.println("\n2)__Using of Logical \"AND (&&)\" Operator with boolean data type:");
+		boolean num1 = x < 5 && x < 10;
+		System.out.println(num1);
+		boolean num2 = x > 5 && x > 10;
+		System.out.println(num2);
+		boolean num3 = x > 19 && x > 20;
+		System.out.println(num3);
 		
-		//Example of LOGICAL AND '&&' with Selection Statements (if/else if/else):
+		// Example of Logical "AND (&&)" Operator with selection statements (if, else if, else):
+		System.out.println("\n3)__Using Logical \"AND (&&)\" Operator with Selection Statement (if, else if, else):");
 		if (x < 5 && x < 10) {
-			System.out.println("If block is: TRUE");
+			System.out.println("if block is: TRUE");
 		}else if(x > 5 && x > 10){
-			System.out.println("Else if block is: TRUE");
+			System.out.println("else if block is: TRUE");
 		}else {
 			System.out.println("Look both sides of Logical And");
 		}
-		//Example of LOGICAL AND '&&' with boolean data type:
-		boolean num1 = x < 5 && x < 10;
-		System.out.println("Depending on the condition of both sides of Logical AND, the result is: " + num1);
-		boolean num2 = x > 5 && x > 10;
-		System.out.println("Depending on the condition of both sides of Logical AND, the result is: " + num2);
-		boolean num3 = x > 19 && x > 20;
-		System.out.println("Depending on the condition of both sides of Logical AND, the result is: " + num3);
-		
-		
+
 		/**
-		 * 2) >>> LOGICAL OR '&& EXAMPLES:'
-		 * As explained above, To return 'true' we need to have one or both sides of the Logical OR '||' operator to be true.
-		 * if one or both sides are true it will return 'true'. 
-		 * Means that we only need one side true to return true.
-		 * Examples are shown below:
+		 * 2) Logical "OR (||) Operator":
+		 * NOTE:
+		 * to return "true" we need to have one or both sides of the Logical "OR (||)" Operator to be true.
+		 * If both sides of the Logical "OR (||)" are "false", it will return "false";
+		 * 
+		 * Following are the examples of Logical "OR (||)" Operator:
 		 */
 		
 		int y = 50;
-		//Examples with simple printout:
-		System.out.println("\nReturn results for Logical OR '||:");
+		// Examples of Logical "OR (||)" Operator with simple printout:
+		System.out.println("\n4)__Using of Logical \"OR (||)\" Operator:");
 		System.out.println(y > 30 || y < 30);
 		System.out.println(y < 30 || y > 30);
 		System.out.println(y > 30 || y > 40);
-		//Example of LOGICAL AND '&&' with Selection Statements (if/else if/else):
-		if (y < 50 || 50 > y) {
-			System.out.println("If block is: TRUE");
-		}else if(y > 70 || y > 60){
-			System.out.println("Else if block is: TRUE");
-		}else {
-			System.out.println("'False' Look both sides of Logical OR");
-		}
-		//Example of LOGICAL AND '&&' with boolean data type:
+	
+		// Example of Logical "AND (&&)" Operator with boolean data type:
+		System.out.println("\n5)__Using of Logical \"OR (||)\" Operator with boolean data type:");
 		boolean numA = y < 100 || y < 40;
-		System.out.println("Depending on the condition of both sides of Logical OR, the result is: " + numA);
+		System.out.println(numA);
 		boolean numB = y > 60 || y > 50;
-		System.out.println("Depending on the condition of both sides of Logical OR, the result is: " + numB);
+		System.out.println(numB);
 		boolean numC = y > 30 || y > 40;
-		System.out.println("Depending on the condition of both sides of Logical OR, the result is: " + numC);
+		System.out.println(numC);
 		
 		/**
-		 * 3) >>> LOGICAL NOT '!' EXAMPLES:
-		 * As explained above, Logical Not '!' operator reverses the result. It returns false if the result is true.
-		 * Basically reverses the result of boolean. For instance, if the return result for other logical operators is true and you add 
-		 * the Logical Not '!' to it, the return results will reverse from true to false. 
-		 * Means that we only need one side true to return true.
+		 * 3) Logical "NOT (!)" Operator:
+		 * NOTE:
+		 * To return "true" we only need to have one side of the Logical "NOT (!)" Operator to be "true".
+		 * Logical "NOT (!)" Operator reverses the result. It returns "false" if the result is "true".
+		 * Basically, reverses the result of boolean. For instance, if the return result for other Logical Operators is "true" 
+		 * and you add the Logical "NOT (!)" to it, the return results will get reverse from "true" to "false". 
+		 * 
 		 * Examples are shown below:
 		 */
 		
 		int z = 40;
-		//Examples of LOGICAL NOT '!' with simple printout:
-		System.out.println("\nReturn results for Logical NOT '!':");
+		// Examples of Logical "NOT (!)" with simple printout:
+		System.out.println("\n6)__Return results for Logical \"NOT (!)\":");
 		System.out.println(!(z > 50 || 30 < z));
 		System.out.println(!(60 != z && z != 70));
 		System.out.println(z > 30 || 30 > z);
-		//Example of LOGICAL NOT '!' with Selection Statements (if/else if/else):
+		// Example of Logical "NOT (!)" with Selection Statements (if, else if, else):
+		System.out.println("\n7)__Return result for Logical \"NOT (!)\" with Selection Statements (if, else if, else):");
 		if (!(z < 20 || 30 > z)) {
-			System.out.println("If block is: TRUE");
+			System.out.println("if block is: TRUE");
 		}else if(70 > z && z > 50){
-			System.out.println("Else if block is: TRUE");
+			System.out.println("else if block is: TRUE");
 		}else {
 			System.out.println("Look both sides of Logical OR");
 		}
-		//Example of LOGICAL NOT '!' with boolean data type:
+		// Example of Logical "NOT (!)" with boolean data type:
+		System.out.println("\n8)__Return results for Logical \"NOT (!)\" with boolean data type:");
 		boolean num4 = 10 < z || z != 20;
-		System.out.println("Depending on the condition of both sides of Logical NOT, the result is: " + num4);
+		System.out.println(num4);
 		boolean num5 = !(20 != 50 && 30 != 20);
-		System.out.println("Depending on the condition of both sides of Logical NOT, the result is: " + num5);
+		System.out.println(num5);
 		boolean num6 = !(20 > z && z > 20);
-		System.out.println("Depending on the condition of both sides of Logical NOT, the result is: " + num6);
+		System.out.println(num6);
 		
 		/**
 		 * 4) >>> LOGICAL XOR '^'(EXCLUSIVE OR) EXAMPLES:

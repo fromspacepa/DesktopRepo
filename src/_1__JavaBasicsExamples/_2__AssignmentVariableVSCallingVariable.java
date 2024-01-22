2fd44c3fa0fc233080eb73c9d732e8b438bc28b0
fromspacepa <fhamed932@gmail.com>
2024-01-22 13:42:53 -0500

    LaptopUpdates

--- src/_1__JavaBasicsExamples/_2__AssignmentVariableVSCallingVariable.java
+++ src/_1__JavaBasicsExamples/_2__AssignmentVariableVSCallingVariable.java
@@ -1,6 +1,6 @@
 package _1__JavaBasicsExamples;
 
-public class _2__Operators_AssignmentVSCallingVariable {
+public class _2__AssignmentVariableVSCallingVariable {
 
 	public static void main(String[] args) {
 		

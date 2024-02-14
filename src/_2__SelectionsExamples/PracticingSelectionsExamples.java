eef2b563c598fd7def2386836cc2c2b660dbc34d
fromspacepa <fhamed932@gmail.com>
2024-02-14 12:02:56 -0500

    LaptopUpdates

--- src/_2__SelectionsExamples/PracticingSelectionsExamples.java
+++ src/_2__SelectionsExamples/PracticingSelectionsExamples.java
@@ -7,17 +7,8 @@ public class PracticingSelectionsExamples {
 	public static void main(String[] args) {
 
 
-		int myValue = 20;
-		
-		if (myValue % 2 == 0) {
-			System.out.println("Even");
-		} else if (myValue % 3 == 0) {
-			System.out.println("Odd");
-		}
-		
-		String myResults = myValue % 3 == 0 ? "Even" : "Odd";
-		System.out.println(myResults);
 
+		
 		}
 
 }
--- src/_2__SelectionsExamples/_1d_NestedIfStatement.java
+++ src/_2__SelectionsExamples/_1d_NestedIfStatement.java
@@ -14,17 +14,27 @@ public class _1d_NestedIfStatement {
 		 * 
 		 */
 
-		int grade = 200;
+		int grade = 59;
 		
 		if (grade >= 90) {
 			System.out.print("You got grade A");
-			if (grade >= 92 && grade <97) 
-				System.out.println("-");
-				if (grade >= 98 && grade >=100) 
-					System.out.println("+");
-				}
+		}if (grade >= 92 && grade <=97) { 
+			System.out.println("-");
+		}if (grade >= 98 && grade >=100) {
+			System.out.println("+");
+		}if (grade >= 80 && grade <= 89) {
+			System.out.println("B");
+		}if (grade >= 70 && grade <= 79) {
+			System.out.println("C");
+		}if (grade >= 60 && grade <= 69) {
+			System.out.println("D");
+		}if (grade >= 50 && grade <= 59) {
+			System.out.println("F \nInorder to achieve graduate, you have to enroll in the credit recovery program.");
 			
 
+		}else {
+			System.out.println("\n You did it!");
+		}
 	}
 
 }
